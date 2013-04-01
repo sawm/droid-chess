4b0eb25192a66b70f4cb61fc234ad521fc74964b
Sidney Akers <Sidneyaks@gmail.com>
2013-04-01 13:26:11 -0500

    Uploaded and committed all Chess piece images

--- droid-chess/gen/com/example/droid_chess/R.java
+++ droid-chess/gen/com/example/droid_chess/R.java
@@ -22,10 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int black=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int pawn=0x7f020003;
-        public static final int white=0x7f020004;
+        public static final int bishop=0x7f020001;
+        public static final int black=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int king=0x7f020004;
+        public static final int knight=0x7f020005;
+        public static final int pawn=0x7f020006;
+        public static final int queen=0x7f020007;
+        public static final int rook=0x7f020008;
+        public static final int white=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
