c1d83e881bcf110e3683dd4cba6ea61e2cfb584e
Sidney Akers <Sidneyaks@gmail.com>
2013-04-02 02:01:44 -0500

    Fixed it so that all pieces appear on the board, also set OnClickListeners for all pieces. Currently the OnClickListener only pops up a toast via an over-ridden onClick function at the bottom. Attempting to find a way to differentiate pieces to calculate appropriate moves.

--- droid-chess/src/com/example/droid_chess/GameActivity.java
+++ droid-chess/src/com/example/droid_chess/GameActivity.java
@@ -2,22 +2,23 @@ package com.example.droid_chess;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements View.OnClickListener {
 
-	private static final String TAG = "MyActivity";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -60,56 +61,80 @@ public class GameActivity extends Activity {
 		
 
 		///////////////////////////////////////////////////////////////////////
-		//Setting up the pieces, only able to set up first row so far
+		//Setting up the pieces
 		///////////////////////////////////////////////////////////////////////
 		ImageView[] white = new ImageView[16];
 		white[0] = new ImageView(this); white[0].setImageResource(R.drawable.rook);
+		white[0].setId(11);
 		white[1] = new ImageView(this);	white[1].setImageResource(R.drawable.knight);
+		white[0].setId(12);
 		white[2] = new ImageView(this);	white[2].setImageResource(R.drawable.bishop);
+		white[0].setId(13);
 		white[3] = new ImageView(this);	white[3].setImageResource(R.drawable.king);
+		white[0].setId(14);
 		white[4] = new ImageView(this);	white[4].setImageResource(R.drawable.queen);
+		white[0].setId(15);
 		white[5] = new ImageView(this);	white[5].setImageResource(R.drawable.bishop);
+		white[0].setId(16);
 		white[6] = new ImageView(this);	white[6].setImageResource(R.drawable.knight);
-		for (int x = 7; x<16 ; x++){
-			white[x] = new ImageView(this);	white[x].setImageResource(R.drawable.pawn);			
-		}
+		white[0].setId(17);
+		white[7] = new ImageView(this); white[7].setImageResource(R.drawable.rook);
+		white[0].setId(18);
 
+		ImageView[] black = new ImageView[16];
+		black[0] = new ImageView(this); black[0].setImageResource(R.drawable.rook);
+		black[0].setId(21);
+		black[1] = new ImageView(this);	black[1].setImageResource(R.drawable.knight);
+		black[0].setId(22);
+		black[2] = new ImageView(this);	black[2].setImageResource(R.drawable.bishop);
+		black[0].setId(23);
+		black[3] = new ImageView(this);	black[3].setImageResource(R.drawable.queen);
+		black[0].setId(24);
+		black[4] = new ImageView(this);	black[4].setImageResource(R.drawable.king);
+		black[0].setId(25);
+		black[5] = new ImageView(this);	black[5].setImageResource(R.drawable.bishop);
+		black[0].setId(26);
+		black[6] = new ImageView(this);	black[6].setImageResource(R.drawable.knight);
+		black[0].setId(27);
+		black[7] = new ImageView(this); black[7].setImageResource(R.drawable.rook);
+		black[0].setId(28);
+		
+		for (int x = 8; x<16 ; x++){
+			white[x] = new ImageView(this);	white[x].setImageResource(R.drawable.pawn);	
+			white[0].setId(19);
+			black[x] = new ImageView(this);	black[x].setImageResource(R.drawable.pawn);	
+			black[0].setId(29);	
+		}
+		
+		for (int x = 0 ; x<16 ; x++){
+			white[x].setColorFilter(0x88ffffff,PorterDuff.Mode.SRC_ATOP);
+			black[x].setColorFilter(0x88000000,PorterDuff.Mode.SRC_ATOP);
+			white[x].setOnClickListener(this);
+			black[x].setOnClickListener(this);
+			
+		}
+		
 		position.x = 0;
 		position.y = 0;
-		//for (int y = 0; y < 2; y++ ){
-			for (int x = 0; x < 8; x++) {
+			for (int x = 0; x < 16; x++) {
 				RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
 				pieceParams.leftMargin=position.x;
 				pieceParams.topMargin=position.y;
+				position.x = (width / 8) * ((x+1) % 8);
+				position.y = (width / 8) * ((int) Math.floor((x+1) / 8));
 				layout.addView(white[x],pieceParams);
-				position.x += width / 8;
 			}
-			//position.x = 0;
-			//position.y = width / 8;
-		//}
-		
 
-		///////////////////////////////////////////////////////////////////////	
-		//experiment code to test colorfilters, will be removed upon completion
-		///////////////////////////////////////////////////////////////////////
-		ImageView rook = new ImageView(this);
-		rook.setImageResource(R.drawable.king);
-		rook.setColorFilter(0xff222222,PorterDuff.Mode.SRC_ATOP);
-		RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
-		pieceParams.leftMargin=0;
-		pieceParams.topMargin=0;
-		layout.addView(rook,pieceParams);
-		
-		
-		///////////////////////////////////////////////////////////////////////
-		//Set the pieces
-		///////////////////////////////////////////////////////////////////////
-		/*ImageView[] white = new ImageView[16];
-		white[0].setImageResource(R.drawable.rook);
-		RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
-		pieceParams.leftMargin=0;
-		pieceParams.topMargin=0;
-		layout.addView(white[0],pieceParams);*/
+		position.x = 0;
+		position.y = 7 *(width/8);
+			for (int x = 0; x < 16; x++) {
+				RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
+				pieceParams.leftMargin=position.x;
+				pieceParams.topMargin=position.y;
+				position.x = (width / 8) * ((x+1) % 8);
+				position.y = (7 *(width/8)) - ((width / 8) * ((int) Math.floor((x+1) / 8)));
+				layout.addView(black[x],pieceParams);
+			}
 		
 		
 		setupActionBar();
@@ -147,5 +172,13 @@ public class GameActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+
+	@Override
+	public void onClick(View view) {
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast.makeText(context, "" + view.getId(), duration).show();
 	}
 }
