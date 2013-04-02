7243560937b0aa86f4b8486c5366f60c5796088e
Will <wmaxwell90@gmail.com>
2013-04-02 16:02:02 -0500

    Update code to make piece positions match array indices

--- droid-chess/src/com/example/droid_chess/GameActivity.java
+++ droid-chess/src/com/example/droid_chess/GameActivity.java
@@ -68,45 +68,45 @@ public class GameActivity extends Activity implements View.OnClickListener {
 		///////////////////////////////////////////////////////////////////////
 		white = new ImageView[16];
 		white[0] = new ImageView(this); white[0].setImageResource(R.drawable.rook);
-		white[0].setContentDescription("1:1:1:1");
+		white[0].setContentDescription("1:1:0:0");
 		white[1] = new ImageView(this);	white[1].setImageResource(R.drawable.knight);
-		white[1].setContentDescription("1:2:2:1");
+		white[1].setContentDescription("1:2:1:0");
 		white[2] = new ImageView(this);	white[2].setImageResource(R.drawable.bishop);
-		white[2].setContentDescription("1:3:3:1");
+		white[2].setContentDescription("1:3:2:0");
 		white[3] = new ImageView(this);	white[3].setImageResource(R.drawable.king);
-		white[3].setContentDescription("1:4:4:1");
+		white[3].setContentDescription("1:4:3:0");
 		white[4] = new ImageView(this);	white[4].setImageResource(R.drawable.queen);
-		white[4].setContentDescription("1:5:5:1");
+		white[4].setContentDescription("1:5:4:0");
 		white[5] = new ImageView(this);	white[5].setImageResource(R.drawable.bishop);
-		white[5].setContentDescription("1:3:6:1");
+		white[5].setContentDescription("1:3:5:0");
 		white[6] = new ImageView(this);	white[6].setImageResource(R.drawable.knight);
-		white[6].setContentDescription("1:2:7:1");
+		white[6].setContentDescription("1:2:6:0");
 		white[7] = new ImageView(this); white[7].setImageResource(R.drawable.rook);
-		white[7].setContentDescription("1:1:8:1");
+		white[7].setContentDescription("1:1:7:0");
 
 		black = new ImageView[16];
 		black[0] = new ImageView(this); black[0].setImageResource(R.drawable.rook);
-		black[0].setContentDescription("2:1:1:8");
+		black[0].setContentDescription("2:1:0:7");
 		black[1] = new ImageView(this);	black[1].setImageResource(R.drawable.knight);
-		black[1].setContentDescription("2:2:2:8");
+		black[1].setContentDescription("2:2:1:7");
 		black[2] = new ImageView(this);	black[2].setImageResource(R.drawable.bishop);
-		black[2].setContentDescription("2:3:3:8");
+		black[2].setContentDescription("2:3:2:7");
 		black[3] = new ImageView(this);	black[3].setImageResource(R.drawable.queen);
-		black[3].setContentDescription("2:5:4:8");
+		black[3].setContentDescription("2:5:3:7");
 		black[4] = new ImageView(this);	black[4].setImageResource(R.drawable.king);
-		black[4].setContentDescription("2:4:5:8");
+		black[4].setContentDescription("2:4:4:7");
 		black[5] = new ImageView(this);	black[5].setImageResource(R.drawable.bishop);
-		black[5].setContentDescription("2:3:6:8");
+		black[5].setContentDescription("2:3:5:7");
 		black[6] = new ImageView(this);	black[6].setImageResource(R.drawable.knight);
-		black[6].setContentDescription("2:2:7:8");
+		black[6].setContentDescription("2:2:6:7");
 		black[7] = new ImageView(this); black[7].setImageResource(R.drawable.rook);
-		black[7].setContentDescription("2:1:8:8");
+		black[7].setContentDescription("2:1:7:7");
 		
 		for (int x = 8; x<16 ; x++){
 			white[x] = new ImageView(this);	white[x].setImageResource(R.drawable.pawn);	
-			white[x].setContentDescription("1:6:" + (x-7) + ":2");
+			white[x].setContentDescription("1:6:" + (x-8) + ":1");
 			black[x] = new ImageView(this);	black[x].setImageResource(R.drawable.pawn);	
-			black[x].setContentDescription("2:6:" + (x-7) + ":7");	
+			black[x].setContentDescription("2:6:" + (x-8) + ":6");	
 		}
 		
 		for (int x = 0 ; x<16 ; x++){
@@ -192,6 +192,7 @@ public class GameActivity extends Activity implements View.OnClickListener {
 		Context context = getApplicationContext();
 		String message = "";
 		int duration = Toast.LENGTH_SHORT;
+		Point piecePos = new Point();
 		//Toast.makeText(context, "" + view.getContentDescription(), duration).show();
 		
 		if (getPieceParams(view,0) == 1) {
@@ -205,6 +206,12 @@ public class GameActivity extends Activity implements View.OnClickListener {
 		switch (getPieceParams(view,1)){
 		case 1:
 			Toast.makeText(context, message + " Rook (" + getPieceParams(view,2) + "," + getPieceParams(view,3) + ")", duration).show();
+//			piecePos.x = getPieceParams(view, 2)-1;
+//			piecePos.y = getPieceParams(view, 3)-1;
+//			for(int x = piecePos.x; x < 8; x++){
+//				squareArray[x]
+//			}
+//			for(int x = piecePos.x; x >= 0; x--)
 			break;
 		case 2:
 			Toast.makeText(context, message + " Knight (" + getPieceParams(view,2) + "," + getPieceParams(view,3) + ")", duration).show();
