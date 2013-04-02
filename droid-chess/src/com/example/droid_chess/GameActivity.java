f04847c65b5392aaad4153694d4561be324d36be
Sidney Akers <Sidneyaks@gmail.com>
2013-04-01 21:30:32 -0500

    Update chess piece graphics so they have reasonable height variences,

--- droid-chess/src/com/example/droid_chess/GameActivity.java
+++ droid-chess/src/com/example/droid_chess/GameActivity.java
@@ -3,30 +3,38 @@ package com.example.droid_chess;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Display;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+
 
 public class GameActivity extends Activity {
 
+	private static final String TAG = "MyActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-
+		
+		///////////////////////////////////////////////////////////////////////
+		//Get width of the screen to properly calculate board size
+		///////////////////////////////////////////////////////////////////////
 		Display display = getWindowManager().getDefaultDisplay();
 		Point size = new Point();
 		display.getSize(size);
 		int width = size.x;
-
+		
+		///////////////////////////////////////////////////////////////////////
+		//Create the board layout
+		///////////////////////////////////////////////////////////////////////
 		RelativeLayout layout = (RelativeLayout) findViewById(R.id.screen);
 		Point position = new Point();
 		position.x = 0;
@@ -49,7 +57,62 @@ public class GameActivity extends Activity {
 			position.x = 0;
 			position.y += width / 8;
 		}
+		
+		
+		ImageView[] white = new ImageView[16];
+		white[0] = new ImageView(this);
+		white[0].setImageResource(R.drawable.rook);
+		white[1] = new ImageView(this);
+		white[1].setImageResource(R.drawable.knight);
+		white[2] = new ImageView(this);
+		white[2].setImageResource(R.drawable.bishop);
+		white[3] = new ImageView(this);
+		white[3].setImageResource(R.drawable.king);
+		white[4] = new ImageView(this);
+		white[4].setImageResource(R.drawable.queen);
+		white[5] = new ImageView(this);
+		white[5].setImageResource(R.drawable.bishop);
+		white[6] = new ImageView(this);
+		white[6].setImageResource(R.drawable.knight);
+		for (int x = 7; x<16 ; x++){
+			white[x] = new ImageView(this);
+			white[x].setImageResource(R.drawable.pawn);			
+		}
 
+		position.x = 0;
+		position.y = 0;
+		for (int y = 0; y < 2; y++ ){
+			for (int x = 0; x < 8; x++) {
+				RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
+				pieceParams.leftMargin=position.x;
+				pieceParams.topMargin=position.y;
+				layout.addView(white[x],pieceParams);
+				position.x += width / 8;
+			}
+			//position.x = 0;
+			//position.y = width / 8;
+		}
+		
+		ImageView rook = new ImageView(this);
+		rook.setImageResource(R.drawable.king);
+		rook.setColorFilter(0xff222222,PorterDuff.Mode.SRC_ATOP);
+		RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
+		pieceParams.leftMargin=0;
+		pieceParams.topMargin=0;
+		layout.addView(rook,pieceParams);
+		
+		
+		///////////////////////////////////////////////////////////////////////
+		//Set the pieces
+		///////////////////////////////////////////////////////////////////////
+		/*ImageView[] white = new ImageView[16];
+		white[0].setImageResource(R.drawable.rook);
+		RelativeLayout.LayoutParams pieceParams = new RelativeLayout.LayoutParams(width/8,width/8);
+		pieceParams.leftMargin=0;
+		pieceParams.topMargin=0;
+		layout.addView(white[0],pieceParams);*/
+		
+		
 		setupActionBar();
 	}
 
